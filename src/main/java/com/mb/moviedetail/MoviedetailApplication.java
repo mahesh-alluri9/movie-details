eaf7606454f49e46292f03bace3df09ee883d89c
Mahesh <maheshalluri09@gmail.com>
2024-08-24 08:26:59 +0530

    Code Pushed

---
+++ src/main/java/com/mb/moviedetail/MoviedetailApplication.java
@@ -0,0 +1,13 @@
+package com.mb.moviedetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedetailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mb/moviedetail/MoviedetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.moviedetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
